ec56b3caa62b807956519d2751f04768a3a58b66
Ganesh <134919900+kganesh09@users.noreply.github.com>
2025-06-26 18:04:21 +0530

    emoji added stage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
       <div className="content">
         <h1>Welcome to the Home Page</h1>
         <p>This is a protected route, accessible only after login.</p>
-        <p>This is a Ganesh work</p>
+        <p>This is a Ganesh work ✅</p>
       </div>
     </div>
   );
