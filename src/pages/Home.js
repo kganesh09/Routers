d19bd6069d092cab557a23b3b37332e35cebc972
Ganesh <134919900+kganesh09@users.noreply.github.com>
2025-06-26 17:47:01 +0530

    commit message before edit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,7 @@ const Home = () => {
         <h1>Welcome to the Home Page</h1>
         <p>This is a protected route, accessible only after login.</p>
         <p>This is a Ganesh work</p>
+        <p>git commit</p>
       </div>
     </div>
   );
