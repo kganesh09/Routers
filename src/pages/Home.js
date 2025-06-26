b671b4a97fe18681a336b50af98df9221fdf8b8c
Ganesh <134919900+kganesh09@users.noreply.github.com>
2025-06-26 17:56:01 +0530

    removerd the unwanted line

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,6 @@ const Home = () => {
         <h1>Welcome to the Home Page</h1>
         <p>This is a protected route, accessible only after login.</p>
         <p>This is a Ganesh work</p>
-        <p>git commit</p>
       </div>
     </div>
   );
